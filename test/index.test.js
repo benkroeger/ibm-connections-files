9ac0fcb065751f4c6a5409c2a0efca92af17018b
ajevtic <ajevtic@ztech.io>
2017-08-11 14:54:47 +0200

    methods split into separate files main index file modified

--- test/index.test.js
+++ test/index.test.js
@@ -9,7 +9,7 @@ const test = require('ava');
 const _ = require('lodash');
 
 // internal modules
-const FileService = require('../lib');
+const fileService = require('../lib');
 const { mockHttpRequests, cleanAll, writeNockCallsToFile, recordHttpRequests } = require('./helpers/http-mocking-utils');
 
 // configure dotenv
@@ -40,9 +40,9 @@ test.beforeEach((t) => {
   const foldersListProperties = [...baseProperties, 'uuid', 'visibility', 'type',
     'isSyncable', 'allowSetFavorite', 'allowFollowing'];
 
-  const source = new FileService('https://apps.na.collabserv.com/files', serviceOptions);
+  const service = fileService('https://apps.na.collabserv.com/files', serviceOptions);
   _.assign(t.context, {
-    source,
+    service,
     serviceOptions,
     secondLvlProperties,
     thirdLvlProperties,
@@ -68,13 +68,11 @@ test.beforeEach((t) => {
 // });
 
 /* Successful scenarios validations */
-test.cb('validate retrieving personal files feed', (t) => {
-  const { source, filesProperties, secondLvlProperties, thirdLvlProperties } = t.context;
-  const params = {
-    authType: 'basic',
-  };
 
-  source.feeds.myFiles(params, (err, response) => {
+test.cb('validate retrieving personal files feed', (t) => {
+  const { service, filesProperties, secondLvlProperties, thirdLvlProperties } = t.context;
+
+  service.myFiles({}, {}, (err, response) => {
     const { files } = response;
     t.true(_.isArray(files), '{response.files} should be an array');
     t.is(files.length, 5, 'there should be exactly 5 elements in {response.files}');
@@ -92,16 +90,16 @@ test.cb('validate retrieving personal files feed', (t) => {
 });
 
 test.cb('validate retrieving files from community feed', (t) => {
-  const { source, communityFilesProperties, secondLvlProperties, thirdLvlProperties } = t.context;
-  const params = {
+  const { service, communityFilesProperties, secondLvlProperties, thirdLvlProperties } = t.context;
+  const query = {
     authType: 'basic',
     communityId: '5dd83cd6-d3a5-4fb3-89cd-1e2c04e52250',
   };
 
-  source.feeds.communityFiles(params, (err, response) => {
+  service.communityFiles(query, {}, (err, response) => {
     const { files } = response;
     t.true(_.isArray(files), '{response.files} should be an array');
-    t.is(files.length, 117, 'there should be exactly 117 elements in {response.files}');
+    t.is(files.length, 10, 'there should be exactly 10 elements in {response.files}');
     files.forEach((file, i) => {
       communityFilesProperties.forEach(prop => t.true(prop in file, `[${prop}] should be a member of response.files[${i}]`));
       [...secondLvlProperties, 'addedBy'].forEach(prop => t.true(_.isPlainObject(file[prop]), `[${prop}] should be a plain object, instead we got: [${typeof file[prop]}]`));
@@ -117,12 +115,9 @@ test.cb('validate retrieving files from community feed', (t) => {
 });
 
 test.cb('validate retrieving publicFiles feed', (t) => {
-  const { source, filesProperties, secondLvlProperties, thirdLvlProperties } = t.context;
-  const params = {
-    authType: 'basic',
-  };
+  const { service, filesProperties, secondLvlProperties, thirdLvlProperties } = t.context;
 
-  source.feeds.publicFiles(params, (err, response) => {
+  service.publicFiles({}, {}, (err, response) => {
     const { files } = response;
     t.true(_.isArray(files), '{response.files} should be an array');
     t.is(files.length, 2, 'there should be exactly 2 elements in {response.files}');
@@ -141,16 +136,16 @@ test.cb('validate retrieving publicFiles feed', (t) => {
 });
 
 test.cb('validate retrieving all files from folder feed', (t) => {
-  const { source, filesProperties, secondLvlProperties, thirdLvlProperties } = t.context;
-  const params = {
+  const { service, filesProperties, secondLvlProperties, thirdLvlProperties } = t.context;
+  const query = {
     authType: 'basic',
     collectionId: '2e53fe56-84f6-485f-8b7a-0429f852f015',
   };
 
-  source.feeds.filesFromFolder(params, (err, response) => {
+  service.filesFromFolder(query, {}, (err, response) => {
     const { files } = response;
     t.true(_.isArray(files), '{response.files} should be an array');
-    t.is(files.length, 117, 'there should be exactly 117 elements in {response.files}');
+    t.is(files.length, 10, 'there should be exactly 10 elements in {response.files}');
     files.forEach((file, i) => {
       filesProperties.forEach(prop => t.true(prop in file, `[${prop}] should be a member of response.files[${i}]`));
       secondLvlProperties.forEach(prop => t.true(_.isPlainObject(file[prop]), `[${prop}] should be a plain object, instead we got: [${typeof file[prop]}]`));
@@ -160,27 +155,6 @@ test.cb('validate retrieving all files from folder feed', (t) => {
 
       const { links: { enclosure: { length } } } = file;
       t.true(!!parseInt(length, 10), `length: [${length}] should be a number`);
-    });
-    t.end();
-  });
-});
-
-test.cb('validate retrieving list of folders feed', (t) => {
-  const { source, foldersListProperties, secondLvlProperties, thirdLvlProperties } = t.context;
-  const params = {
-    authType: 'basic',
-  };
-
-  source.feeds.foldersList(params, (err, response) => {
-    const { folders } = response;
-    t.true(_.isArray(folders), '{response.folders} should be an array');
-    t.is(folders.length, 1, 'there should be only 1 element in {response.folders}');
-    folders.forEach((folder, i) => {
-      foldersListProperties.forEach(prop => t.true(prop in folder, `[${prop}] should be a member of response.folders[${i}]`));
-      secondLvlProperties.forEach(prop => t.true(_.isPlainObject(folder[prop]), `[${prop}] should be a plain object, instead we got: [${typeof folder[prop]}]`));
-      ['author', 'modifier'].forEach((obj) => {
-        thirdLvlProperties.forEach(prop => t.true(prop in folder[obj], `[${prop}] should be a member of folder[${obj}]`));
-      });
     });
     t.end();
   });
